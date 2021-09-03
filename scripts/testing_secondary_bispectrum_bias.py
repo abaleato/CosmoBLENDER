1e6cdf985332b406e6b589b3e60b1bf7ab5d5170
Antón Baleato Lizancos <ab2368@cam.ac.uk>
2021-09-03 10:52:55 +0200

    added .png´s to .gitignore

--- scripts/testing_secondary_bispectrum_bias.py
+++ scripts/testing_secondary_bispectrum_bias.py
@@ -25,9 +25,9 @@ if __name__ == '__main__':
     cosmoParams = {'As': 2.4667392631170437e-09, 'ns': .96, 'omch2': (0.25 - .043) * .7 ** 2, 'ombh2': 0.044 * .7 ** 2,
                    'H0': 70.}  # Note that for now there is still cosmology dpendence in the cls defined within the experiment class
 
-    nZs = 30
-    nMasses = 30
-    bin_width_out_second_bispec_bias = 60
+    nZs = 3 #30
+    nMasses = 3 #30
+    bin_width_out_second_bispec_bias = 1000 #60
 
     # Initialise a halo model object for the calculation, using mostly default parameters
     hm_calc = biases.hm_framework(cosmoParams=cosmoParams, nZs=nZs, nMasses=nMasses)
