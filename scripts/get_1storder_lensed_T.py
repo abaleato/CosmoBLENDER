c0bb7a388750c0d2b5866c8ae79672bcd6994fab
Anton Baleato <ab2368@cam.ac.uk>
2025-03-12 21:42:26 -0700

    bugfixes

--- scripts/get_1storder_lensed_T.py
+++ scripts/get_1storder_lensed_T.py
@@ -1,5 +1,4 @@
 # Script to generate the first order correction to the temperature due to lensing
-
 import numpy as np
 import healpy as hp
 import csbt
@@ -24,14 +23,27 @@ def kappa_map_phi_to_phi_alm(kappa_map, ls, lmax):
 
 
 if __name__ == '__main__':
-    lmax = 3000#5000
+    lmax = 3000 #5000
+    which_sim = 'agora' # 'websky' or 'agora'
 
-    websky_dir = '/Volumes/TOSHIBA_EXT/data/sims/websky/'
-    websky_unlensed_cmb_alm = shorten_alm(hp.read_alm(websky_dir + 'unlensed_alm.fits'), lmax)
-    true_kappa = hp.read_map(websky_dir + 'kap.fits')
+    if which_sim == 'websky':
+        out_dir = '/pscratch/sd/a/ab2368/websky/'
+        sim_dir = out_dir
+        kappa_dir = sim_dir + 'kap.fits'
+        unl_alm_path = sim_dir + 'unlensed_alm.fits'
+    elif which_sim == 'agora':
+        out_dir = '/pscratch/sd/a/ab2368/agora/'
+        sim_dir = '/global/cfs/projectdirs/cmb/data/agora/components/'
+        kappa_dir = sim_dir+'cmbkappa/agora_born_cmbkappa_highzadded_lowLcorrected.fits'
+        unl_alm_path = sim_dir+'cmb/unl/teb1/agora_phiG_teb1_seed1.alm'
+        
+
+    unlensed_cmb_alm = shorten_alm(hp.read_alm(unl_alm_path, hdu=[1]), lmax)
+    true_kappa = hp.read_map(kappa_dir)
     phi_alm = kappa_map_phi_to_phi_alm(true_kappa, np.arange(lmax), lmax)
 
-    T_template = csbt.weights.T_template_weights(np.ones(lmax + 1)).eval_fullsky(phi_alm, websky_unlensed_cmb_alm)
+    T_template = csbt.weights.T_template_weights(np.ones(lmax + 1)).eval_fullsky(phi_alm, unlensed_cmb_alm)
     g_T_alm, c_T_alm = csbt.shts.util.vlm2alm(T_template)
-    np.save('g_T_alm_first_order_in_lensing_lmax{}'.format(lmax), g_T_alm)
+    np.save(out_dir+'g_T_alm_first_order_in_lensing_lmax{}'.format(lmax), g_T_alm)
+    print('Done here')
 
